f9d3aabf1535bff236f085cce42887b004e8c6e7
korolipliaja <dimagolub554@gmail.com>
2025-01-06 21:54:50 +0200

    modified:   .vscode/settings.json 	new file:   projects/training/27.return 	modified:   projects/training/27.return.cpp

--- projects/training/27.return.cpp
+++ projects/training/27.return.cpp
@@ -1,1 +1,26 @@
 #include <iostream>
+
+double arealine(double line);
+double cubeline(double lineC);
+
+using namespace std;
+
+int main(){
+
+    double line = 6.1;
+    double result = arealine(line);
+    double lineC = 6.1;
+    double resultC = cubeline(lineC);
+
+    cout << "Area is: " << result << "cm^2\n"; 
+    cout << "Cube is: " << resultC << "cm^3\n"; 
+    return 0;
+}
+
+double arealine(double line){
+    return line * line;
+}
+
+double cubeline(double lineC){
+    return lineC * lineC * lineC; 
+}
