c767424b4613f8141f03b7c9091e6b3e3843cb36
korolipliaja <dimagolub554@gmail.com>
2025-01-14 21:50:06 +0200

    new file:   projects/bank_program/bank 	modified:   projects/bank_program/bank.cpp

--- projects/bank_program/bank.cpp
+++ projects/bank_program/bank.cpp
@@ -5,66 +5,70 @@ using namespace std;
 
 void balanceATM(double balance);
 double depozitATM();
-double witdhrawATM(double balance); 
-
+double withrawATM(double balance);
 
 int main(){
+
     int choise = 0;
-    double balance = 100.01; 
+    double balance = 101;
 
-do{
-    cout << "*********BANK PROGRAMM*********\n";
-    cout << "*******ENTER YOUR CHOISE*******\n";
-    cout << "1) SHOW BALANCE\n";
-    cout << "2) DEPOZIT MONEY\n";
-    cout << "3) WITHRAW MONEY\n";
-    cout << "4) EXIT\n";
-    cin >> choise;
+    
 
-    switch(choise){
-        case 1: balanceATM(balance);
-            break;
-        case 2: balance = balance + depozitATM();
-            balanceATM(balance);
-            break;
-        case 3: balance = balance - witdhrawATM(balance);
-            balanceATM(balance);
-            break;
-        case 4: cout << "Thank you for viziting ))\n";
-            break;
-        default:cout << "Invalid choise!!!\n";
+    do{
+        cout << "bank accout\n"; 
+        cout << "enter otions\n";
+        cout << "1. check balance\n";
+        cout << "2. depozit\n";
+        cout << "3. withraw\n";
+        cout << "4. exit\n";
+        cin >> choise;    
+        
+        switch(choise){
+            case 1: balanceATM(balance);
+                break; 
+            case 2: balance = balance + depozitATM();
+                balanceATM(balance);
+                break;
+            case 3: balance = balance - withrawATM(balance);
+                balanceATM(balance);
+                break;
+            case 4: cout << "see you soon\n";
+                break;
+            default: cout << "enter a # \n";
     }
-
-}while(choise != 4);
-
+    }while (choise != 4);
+        
     return 0;
 }
 
 void balanceATM(double balance){
-    cout << "Your balance is: " << setprecision(2) << fixed << balance << endl;
+    cout << "your balance is: " << balance << '\n';
 }
 double depozitATM(){
     double amount = 0;
-    cout << "Enter the amout that you want do depozit\n";
-    cin >> amount; 
-    if(amount < 0){
-        cout << "enter a pozitive number\n";
-    }
-    return amount;
-}
-double witdhrawATM(double balance){
-    double amount = 0;
-    cout << "Enter the amout that you want do withraw\n";
+    cout << "how much depozit ?\n";
     cin >> amount;
-    if(amount < balance){
-        cout << "you are enought of money";
+    if(amount > 0){
+        return amount;
+    }
+    else{
+        cout << "enter a valid nubmer\n";
+        return 0;
+    }
+}
+double withrawATM(double balance){
+    double amount = 0;
+    cout << "how much withraw ? \n";
+    cin >> amount;
+    if(amount > balance){
+        cout << "insufficient founds\n"; 
         return 0;
     }
     else if(amount < 0){
-        cout << "that is not valid number\n";
+        cout << "enter a valid number\n";
+        return 0;
     }
     else{
         return amount;
     }
-    return amount; 
 }
