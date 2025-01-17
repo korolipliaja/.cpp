1c9a6113c0ad4be07404ae9d6de8bd9231cc4882
korolipliaja <dimagolub554@gmail.com>
2025-01-17 21:27:22 +0200

    v1.0

--- projects/Euclid/find_p.cpp
+++ projects/Euclid/find_p.cpp
@@ -2,21 +2,63 @@
 #include <cmath> 
 using namespace std;
 
+void rightP(int p);
+void pResult(int p);
+void rootCondition(int p);
+
+int sum = 0;
+
 int main() {
+
     int p;
-    cout << "enter a p from 1 to 127 : ";
+
+    cout << "Enter a p number from 1 to 127: " << '\n';
     cin >> p;
 
-    if (p <= 1 || p % 2 == 0) {
-        cout << "p  << must be greater than 1 and not divisible by 2" << '\n';
-        return 0;
-    }
-
-    double p1Result = pow(2, p - 1);
-    double p2Result = pow(2, p) - 1;
-    double times = p1Result * p2Result;
-
-    cout << "result: " << times << '\n';
+    rightP(p);
+    pResult(p);
+    rootCondition(p);
 
     return 0;
+}
+
+void pResult(int p){
+    double p1Result = pow (2, p - 1);
+    double p2Result = pow (2, p) - 1;
+    sum = p1Result * p2Result;
+    cout << sum << '\n';
+} 
+
+void rightP(int p){
+    if (p <= 1 || p % 2 == 0) {
+        cout << "p  << must be greater than 1 and not divisible by 2" << '\n';
+        exit(0);
+    }
+}
+
+void rootCondition(int p){
+
+    bool hasDivisor;
+    
+    while(true){
+
+        double sqrtResult = sqrt(p);
+        int roundedResult = round(sqrtResult);
+
+
+        for(int i = 2; i < roundedResult; i++){
+            if(p % i == 0){
+                cout << p << " делится на " << i << " без остатка.\n";
+                hasDivisor = true; 
+                break;
+            }
+        }
+
+        if(!hasDivisor){
+            cout << "Все делители дают остаток (исключая 1 и само число). Завершаем программу.\n";
+            break;
+        }else{
+            cout << "Введите новое число, так как деление без остатка было найдено.\n";
+        }
+    }
 }
