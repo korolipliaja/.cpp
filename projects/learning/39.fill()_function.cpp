ff70387779d3ad459da928c5c919d85f2a56881e
korolipliaja <dimagolub554@gmail.com>
2025-01-30 20:55:20 +0200

    rewrite 16 topic

---
+++ projects/learning/39.fill()_function.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm> 
+using namespace std;
+
+int main() {
+    const int SIZE = 100;
+    
+    string foods[SIZE];
+
+    fill(foods, foods + SIZE, "pizza");
+
+    for(string food : foods){
+        cout << food << '\n';
+    }
+
+    return 0;
+}
