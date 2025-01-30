ef545e80ffbc5ba2823add56d18a95bb1ca17e14
korolipliaja <dimagolub554@gmail.com>
2025-01-30 20:08:46 +0200

    rewrite 16 topic

--- projects/learning/16.temperature_convertation.cpp
+++ projects/learning/16.temperature_convertation.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
-
+using namespace std;
 
 int main(){
     double temp;
     char unit;
-    std::cout << "Enter the temperature and unit (C or F): ";
-    std::cin >> temp >> unit;
+    std::cout << "Enter the unit to convert C or F" << '\n';
+    std::cin >> unit;
 
-    if(temp 'C' || temp 'c'){
-        std::cout << temp << 'C' << "is" << temp *1.8 + 32 << 'F' << '\n';
-
-
+    if(unit == 'C' || unit == 'c'){
+        cout << "Enter the temp in Celsius: "; 
+        cin >> temp;
+        temp = (1.8 * temp) + 32.0;
+        cout << "The temp is: " << temp <<  "F\n";
     }
 
+    if(unit == 'F' || unit == 'f'){
+        cout << "Enter the temp in Fahrenheit "; 
+        cin >> temp;
+        temp = (temp - 32) / 1.8; 
+        cout << "The temp is: " << temp <<  "C\n";
+    }
+    else{
+        cout << "Enter value only in C or F";
+    }
 
     return 0;
 }
